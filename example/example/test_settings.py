b773eb4f87700552b3d1f4a81db5fd275a29879c
YoKW <okwrtdsh@gmail.com>
2016-04-13 09:09:49 +0900

    add .travis.yml

---
+++ example/example/test_settings.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travisdb',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
